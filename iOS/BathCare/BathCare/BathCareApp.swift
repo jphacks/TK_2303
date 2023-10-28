aee898a0fd8c642c07fd69a551e5f97176a4a8d0
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2023-10-28 10:45:06 +0900

    initial commit for iOS

---
+++ iOS/BathCare/BathCare/BathCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BathCareApp.swift
+//  BathCare
+//
+//  Created by 五十嵐諒 on 2023/10/28.
+//
+
+import SwiftUI
+
+@main
+struct BathCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
