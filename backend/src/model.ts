b9f286071e14d5402ed49f424118dafa0dc0922e
Sosuke Hosokawa <so298go@gmail.com>
2023-10-28 16:51:18 +0900

    Add backend

---
+++ backend/src/model.ts
@@ -0,0 +1,71 @@
+import { Alart, DeviceStatus, SensorData, SensorDataWithTime } from './types';
+
+const KEY_PREFIX = 'dev:v0';
+type KeyKind = 'status' | 'sensor';
+const generateKey = (id: string, kind: KeyKind) => {
+  return `${KEY_PREFIX}:${kind}:${id}`;
+};
+
+export const getStatus = async (
+  KV: KVNamespace,
+  id: string
+): Promise<DeviceStatus> => {
+  const value = await KV.get(generateKey(id, 'status'));
+  if (!value) {
+    return {
+      status: 'unknown',
+    };
+  }
+
+  const status: DeviceStatus = JSON.parse(value);
+  return status;
+};
+
+export const updateStatus = async (
+  KV: KVNamespace,
+  id: string,
+  status: DeviceStatus
+) => {
+  const value = JSON.stringify(status);
+  await KV.put(generateKey(id, 'status'), value);
+};
+
+export const getSensorData = async (
+  KV: KVNamespace,
+  id: string
+): Promise<SensorDataWithTime> => {
+  const value = await KV.get(generateKey(id, 'sensor'));
+  if (!value) {
+    // TODO: データが入っていない場合の処理
+    return {
+      temperature: 30,
+      humidity: 80,
+      co2: 1000,
+      lastUpdate: '2000-01-01T00:00:00Z',
+    };
+  }
+
+  const data: SensorDataWithTime = JSON.parse(value);
+
+  return data;
+};
+
+export const updateSensorData = async (
+  KV: KVNamespace,
+  id: string,
+  data: SensorData
+) => {
+  const now = new Date();
+  const dateStr = now.toISOString().slice(0, -5) + 'Z';
+
+  const dataWithTime: SensorDataWithTime = { ...data, lastUpdate: dateStr };
+
+  const value = JSON.stringify(dataWithTime);
+  await KV.put(generateKey(id, 'sensor'), value);
+
+  return dataWithTime;
+};
+
+export const sendAlart = async (KV: KVNamespace, id: string, alart: Alart) => {
+  // TODO: アプリへの通知機能を実装
+};
