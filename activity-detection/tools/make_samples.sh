68aa5483e898698934eb36ae0567735162998f7a
Sosuke Hosokawa <so298go@gmail.com>
2023-11-18 15:37:02 +0900

    Edit makefile

--- activity-detection/tools/make_samples.sh
+++ activity-detection/tools/make_samples.sh
@@ -5,10 +5,11 @@ set -eux
 basefile="$1"
 
 tmpwav="tmp.wav"
+duration=8
 for i in {0..10}; do
     trimedfile="samples/sample_$i.wav"
     # trim 8s
-    sox "$basefile" "$tmpwav" trim $(( i * 8 )) $(( (i + 1) * 8 ))
+    sox "$basefile" "$tmpwav" trim $(( i * duration )) $(( (i + 1) * duration ))
     # resample to 8000Hz
     sox "$tmpwav" -r 16000 "$trimedfile"
     python3 tools/to_json.py "$trimedfile" "./samples/sample_$i.json"
